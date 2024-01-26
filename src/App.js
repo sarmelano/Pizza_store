d27b3ae3bb8c4d887f03f111a73731a5296bd752
sarmelano <sarmelano.main@gmail.com>
2024-01-25 16:38:19 -0800

    pages added

--- src/App.js
+++ src/App.js
@@ -2,21 +2,19 @@ import React, { useState } from 'react';
 import './App.css';
 import UserContext from './context/UserContext';
 import Header from './components/Header';
-import Main from './components/LoginPage/Main';
-import { BrowserRouter as Router } from 'react-router-dom';
+import Main from './pages/LoginPage/Main';
+
 
 function App() {
   const [user, setUser] = useState(null);
 
   return (
     <UserContext.Provider value={{ user, setUser }}>
-      <Router>
-        <div className="App">
-          <Header />
-          <Main />
-        </div>
-        {user && console.log(user)} {/* вместо компонента */}
-      </Router>
+      <div className="App">
+        <Header />
+        <Main />
+      </div>
+      {user && console.log(user)} {/* вместо компонента */}
     </UserContext.Provider>
   );
 }
