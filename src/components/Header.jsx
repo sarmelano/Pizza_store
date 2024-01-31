8c587f855f6c94f7bf80822f832dd9f4a53949f6
sarmelano <sarmelano.main@gmail.com>
2024-01-31 15:25:38 -0800

    data renew

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import UserContext from '../context/UserContext'
 
 export default function Header() {
+  const { user } = useContext(UserContext)
+
   return (
     <header className="App-header">
       <p>PIZZA DAY</p>
       <p><input type="text" id="query" placeholder="Search..." /></p>
+      {user && <p>{ user }</p>}
     </header>
   )
 };
--- src/pages/MenuPage/MenuItem.jsx
+++ src/pages/MenuPage/MenuItem.jsx
@@ -4,11 +4,18 @@ import './menu.css'
 export default function MenuItem({ item }) {
   const { name, unitPrice, imageUrl, ingredients, soldOut } = item
   return (
-    <li>
-      <div className='item_img-wrapper'> <img className='item__img' src={imageUrl} alt={name} /></div>
-      <p>{name}</p>
-      <p>{unitPrice} $</p>
-      <button>Add to cart</button>
+    <li className='menu_item'>
+      <div className='item_img-wrapper'>
+        <img className='item__img' src={imageUrl} alt={name} />
+        <p className='item__img-desc'>{ingredients.join('\n')}</p>
+
+
+      </div>
+      <div className='item_desc-wrapper'>
+        <p>{name}</p>
+        <p className='item_price'>{unitPrice} $</p>
+        <button className='item_btn'>Add to cart</button>
+      </div>
     </li>
   )
 };
