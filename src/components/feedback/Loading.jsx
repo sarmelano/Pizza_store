f72a05ab6d8da7ca28a047b9b70e0679667d6e12
sarmelano <sarmelano.main@gmail.com>
2024-02-22 21:24:39 -0800

    loader updated to svg

--- src/components/feedback/Loading.jsx
+++ src/components/feedback/Loading.jsx
@@ -1,5 +1,13 @@
+import './feedback.scss'
+
 function Loading() {
-  return <div>Loading...</div>;
+  return (
+    <div className="loader">
+      <svg className="spinner" width="65px" height="65px" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">
+        <circle className="path" fill="none" strokeWidth="6" strokeLinecap="round" cx="33" cy="33" r="30"></circle>
+      </svg>
+    </div>
+  );
 }
 
 export default Loading;
